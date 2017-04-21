7e65549ebc1a6c6f3b551ae0ebe267bfbb7451c7
Mick Knutson <mickknutson@gmail.com>
2017-04-21 07:34:56 -0400

    Updated local REPO and ran smoke tests

---
+++ StudentWork/code/spring-boot-primer/spring-boot-primer-solution/src/main/java/com/springclass/cloud/PrimerApplication.java
@@ -0,0 +1,12 @@
+package com.springclass.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerApplication.class, args);
+	}
+}
